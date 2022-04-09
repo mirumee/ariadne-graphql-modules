86fd5fe70308226036b77085e319d17b5cd14a51
rafalp <kontakt@rpiton.com>
2022-04-09 15:09:50 +0200

    Bump version to dev2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     license="BSD",
-    version="0.2.0.dev1",
+    version="0.2.0.dev2",
     url="https://github.com/mirumee/ariadne-graphql-modules",
     packages=["ariadne_graphql_modules"],
     include_package_data=True,
