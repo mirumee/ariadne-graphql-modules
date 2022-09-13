f2c0b99df2b13697d928bf4230edaac7cdbcbbb2
Rafał Pitoń <rafal@mirumee.com>
2022-09-13 15:34:52 +0200

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     license="BSD",
-    version="0.7.0.b1",
+    version="0.7.0",
     url="https://github.com/mirumee/ariadne-graphql-modules",
     packages=["ariadne_graphql_modules"],
     include_package_data=True,
